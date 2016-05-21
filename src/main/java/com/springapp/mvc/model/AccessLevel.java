6e8c946b89f9ad996bb79d472b4daa1aa58cb96e
arturfif <arturfif@mail.ru>
2016-05-21 20:18:25 +0300

    first full commit

---
+++ src/main/java/com/springapp/mvc/model/AccessLevel.java
@@ -0,0 +1,11 @@
+package com.springapp.mvc.model;
+
+/**
+ * Created by arturk on 19.05.2016.
+ */
+public enum AccessLevel {
+    READ,
+    READ_WRITE,
+    READ_WRITE_CONTROL
+
+}
---
+++ src/main/java/com/springapp/mvc/model/Document.java
@@ -0,0 +1,11 @@
+package com.springapp.mvc.model;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by arturk on 19.05.2016.
+ */
+
+@Entity
+public class Document {
+}
